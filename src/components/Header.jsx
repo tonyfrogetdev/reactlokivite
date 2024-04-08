3653d19a953d3734fedac2265c8a9ca7f56bd42b
unknown <tonyfrogetdev@gmail.com>
2024-04-08 15:38:23 +0200

    Ajout des plats

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,20 @@
-const Header = () => (
-  <header>
-    <nav>
-      <img
-        src="https://cdn.pixabay.com/photo/2018/03/26/14/18/man-3262834_960_720.png"
-        alt="Logo"
-      />
-      <ul>
-        <li>
-          <a href="/">Accueil</a>
-        </li>
-        <li>Pas l'accueil</li>
-      </ul>
-    </nav>
-  </header>
-);
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <img
+          src="https://cdn.pixabay.com/photo/2018/03/26/14/18/man-3262834_960_720.png"
+          alt="Logo"
+        />
+        <ul>
+          <li>
+            <a href="/">Accueil</a>
+          </li>
+          <li>Pas l'accueil</li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
 export default Header;

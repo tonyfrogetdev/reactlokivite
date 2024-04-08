79579d4f2a2e1ffbe01213bf801ba24f494f367f
unknown <tonyfrogetdev@gmail.com>
2024-04-08 17:00:44 +0200

    boostrap

--- src/components/Dish.jsx
+++ src/components/Dish.jsx
@@ -1,13 +1,14 @@
+import { Card } from "react-bootstrap";
+
 const Dish = ({ title, price, image }) => {
   return (
-    <>
-      <div className="mexicanDish">
-        <h1>{title}</h1>
-
-        <span>{price}</span>
-        <img src={image} alt={title} />
-      </div>
-    </>
+    <Card style={{ width: "18rem", margin: "10px" }}>
+      <Card.Img variant="top" src={image} alt={title} />
+      <Card.Body>
+        <Card.Title>{title}</Card.Title>
+        <Card.Text>Price: {price}</Card.Text>
+      </Card.Body>
+    </Card>
   );
 };
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,29 @@
+import { Navbar, Container, Nav } from "react-bootstrap";
+
 const Header = () => {
   return (
     <header>
-      <nav>
-        <img
-          src="https://cdn.pixabay.com/photo/2018/03/26/14/18/man-3262834_960_720.png"
-          alt="Logo"
-        />
-        <ul>
-          <li>
-            <a href="/">Accueil</a>
-          </li>
-          <li>Pas l'accueil</li>
-        </ul>
-      </nav>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="/">
+            <img
+              src="https://cdn.pixabay.com/photo/2018/03/26/14/18/man-3262834_960_720.png"
+              alt="Logo"
+              width="50"
+              height="50"
+              className="d-inline-block align-top"
+            />
+            Navbar
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav className="ms-auto" navbarScroll>
+              <Nav.Link href="/">Accueil</Nav.Link>
+              <Nav.Link href="#">à Propos</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </header>
   );
 };

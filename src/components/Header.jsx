2d6e08731487a0999f039ed3eb4303fbd723ad68
unknown <tonyfrogetdev@gmail.com>
2024-04-08 15:14:18 +0200

    NavLinkCorrection

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,18 @@
 const Header = () => (
-  <nav>
+  <header>
     <img
       src="https://cdn.pixabay.com/photo/2018/03/26/14/18/man-3262834_960_720.png"
-      alt=""
+      alt="Logo"
     />
-    <ul>
-      <li>Accueil</li>
-      <li>Pas l'accueil</li>
-    </ul>
-  </nav>
+    <nav>
+      <ul>
+        <li>
+          <a href="/">Accueil</a>
+        </li>
+        <li>Pas l'accueil</li>
+      </ul>
+    </nav>
+  </header>
 );
 
 export default Header;

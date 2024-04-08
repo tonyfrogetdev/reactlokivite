e7fa0bc3fb39f4552156a146a5cbf241767cffae
unknown <tonyfrogetdev@gmail.com>
2024-04-08 14:42:18 +0200

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+const Header = () => (
+  <nav>
+    <img
+      src="https://cdn.pixabay.com/photo/2018/03/26/14/18/man-3262834_960_720.png"
+      alt=""
+    />
+    <ul>
+      <li>Accueil</li>
+      <li>Pas l'accueil</li>
+    </ul>
+  </nav>
+);
+
+export default Header;

c7eddb3b6e4e2b6d2d8cc5766041126bfe3122f0
unknown <tonyfrogetdev@gmail.com>
2024-04-09 15:18:56 +0200

    Page non trouvée

--- src/pages/DishDetails.jsx
+++ src/pages/DishDetails.jsx
@@ -1,13 +1,10 @@
 import { Container, Row } from "react-bootstrap";
-import { useParams } from "react-router-dom";
 
 const DishDetails = () => {
-  const { slug } = useParams();
-
   return (
     <Container>
       <Row>
-        <h1>{slug}</h1>
+        <h1>Page non trouvée</h1>
       </Row>
     </Container>
   );

bb3e938a05582c0c05f8df4fc30e528c86cf192c
unknown <tonyfrogetdev@gmail.com>
2024-04-10 12:18:35 +0200

    corrections

--- src/components/Dish.jsx
+++ src/components/Dish.jsx
@@ -1,10 +1,10 @@
 import { Card } from "react-bootstrap";
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 import "../assets/styles/dish.css";
 
 const Dish = ({ title, price, image, slug }) => {
   return (
-    <NavLink to={`/plat/${slug}`} className="nav-link">
+    <Link to={`/plat/${slug}`} className="nav-link">
       <Card>
         <Card.Img className="dish" variant="top" src={image} alt={title} />
         <Card.Body>
@@ -12,7 +12,7 @@ const Dish = ({ title, price, image, slug }) => {
           <Card.Text>Price: {price}</Card.Text>
         </Card.Body>
       </Card>
-    </NavLink>
+    </Link>
   );
 };
 

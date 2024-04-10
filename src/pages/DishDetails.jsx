cddee089235d400a9e78a9fb1b3fec531cd0b223
unknown <tonyfrogetdev@gmail.com>
2024-04-10 17:09:03 +0200

    DishDetails

--- src/pages/DishDetails.jsx
+++ src/pages/DishDetails.jsx
@@ -1,13 +1,28 @@
-import { Container, Row } from "react-bootstrap";
+import { Container, Row, Col, Button } from "react-bootstrap";
 import { useParams } from "react-router-dom";
-
+import dishesData from "../datas/dishes.json";
+import "../assets/styles/dishdetails.css";
 const DishDetails = () => {
   const { slug } = useParams();
 
+  const dish = dishesData.find((dish) => dish.slug === slug);
+
+  if (!dish) {
+    return <div>Plat introuvable</div>;
+  }
+
   return (
     <Container>
-      <Row>
-        <h1>{slug}</h1>
+      <Row className="my-4">
+        <Col className="my-4">
+          <img src={dish.image} alt={dish.title} className="dish" />
+        </Col>
+        <Col className="my-4">
+          <h1>{dish.title}</h1>
+          <p>{dish.description}</p>
+          <p className="fw-bold">Prix : {dish.price}</p>
+          <Button variant="primary">Commander</Button>
+        </Col>
       </Row>
     </Container>
   );
